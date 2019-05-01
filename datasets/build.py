2e6ab4198810219623ded464abacb5c87c3530bd
PGrothaus <philippgrothaus@web.de>
2019-05-01 20:47:48 +0100

    factory for dataset

---
+++ datasets/build.py
@@ -0,0 +1,3 @@
+def build_move_section_only_dataset():
+    # TODO: Implement this (use code from notebook)
+    return 5
---
+++ datasets/factory.py
@@ -0,0 +1,14 @@
+from datasets.build import build_move_section_only_dataset
+
+
+def build(key):
+    build_methods = {
+        "move-section-only": build_move_section_only_dataset,
+    }
+    method = build_methods.get(key)
+    if method:
+        return method()
+    else:
+        print(key)
+        print("No build method defined for this key.")
+        return None
