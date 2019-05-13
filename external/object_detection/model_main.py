34b758207254e6a5a8e75e41fbb36db2eb479ccc
PGrothaus <philippgrothaus@web.de>
2019-05-13 01:40:50 +0100

    building dataset and adding pycocotools

--- external/object_detection/model_main.py
+++ external/object_detection/model_main.py
@@ -18,42 +18,22 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
 from absl import flags
 
 import tensorflow as tf
 
 from object_detection import model_hparams
 from object_detection import model_lib
+from utils.tf_flags import load_object_detection_parameters
+import utils.config as conf
 
-flags.DEFINE_string(
-    'model_dir', None, 'Path to output model directory '
-    'where event and checkpoint files will be written.')
-flags.DEFINE_string('pipeline_config_path', None, 'Path to pipeline config '
-                    'file.')
-flags.DEFINE_integer('num_train_steps', None, 'Number of train steps.')
-flags.DEFINE_boolean('eval_training_data', False,
-                     'If training data should be evaluated for this job. Note '
-                     'that one call only use this in eval-only mode, and '
-                     '`checkpoint_dir` must be supplied.')
-flags.DEFINE_integer('sample_1_of_n_eval_examples', 1, 'Will sample one of '
-                     'every n eval input examples, where n is provided.')
-flags.DEFINE_integer('sample_1_of_n_eval_on_train_examples', 5, 'Will sample '
-                     'one of every n train input examples for evaluation, '
-                     'where n is provided. This is only used if '
-                     '`eval_training_data` is True.')
-flags.DEFINE_string(
-    'hparams_overrides', None, 'Hyperparameter overrides, '
-    'represented as a string containing comma-separated '
-    'hparam_name=value pairs.')
-flags.DEFINE_string(
-    'checkpoint_dir', None, 'Path to directory holding a checkpoint.  If '
-    '`checkpoint_dir` is provided, this binary operates in eval-only mode, '
-    'writing resulting metrics to `model_dir`.')
-flags.DEFINE_boolean(
-    'run_once', False, 'If running in eval-only mode, whether to run just '
-    'one round of eval vs running continuously (default).'
-)
-FLAGS = flags.FLAGS
+
+fp = sys.argv[1]
+conf.loadconfig(fp)
+
+load_object_detection_parameters()
+FLAGS = tf.app.flags.FLAGS
 
 
 def main(unused_argv):
