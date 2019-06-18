ce492b7600af156d4b09513b371fd7bf8b82995f
PGrothaus <philippgrothaus@web.de>
2019-06-18 06:27:31 +0100

    try to remove shuffle

--- models/character-recognition/cnn_with_position.py
+++ models/character-recognition/cnn_with_position.py
@@ -25,12 +25,14 @@ position_map = {"is-first": 0,
                 "is-last": 1,
                 }
 
+BATCH_SIZE = 64
+
 
 def prep_data(key):
     examples = load_generated_moves(key)
 
-    #train_data = load(key)
-    #examples = []
+    # train_data = load(key)
+    # examples = []
     # for k, v in train_data.items():
 #        examples.extend(v)
     plot_example(examples[0])
@@ -57,7 +59,12 @@ def createGenerator(X, I, Y, shuffle=True):
 
     while True:
         # suffled indices
-        idx = np.random.permutation(X.shape[0])
+        if shuffle:
+            print("shuffle")
+            idx = np.random.permutation(X.shape[0])
+        else:
+            print("do not shuffle")
+            idx = np.asarray(list(range(X.shape[0])))
         # create image generator
         datagen = keras.preprocessing.image.ImageDataGenerator(
             featurewise_center=False,  # set input mean to 0 over the dataset
@@ -66,15 +73,16 @@ def createGenerator(X, I, Y, shuffle=True):
             samplewise_std_normalization=False,  # divide each input by its std
             zca_whitening=False,  # apply ZCA whitening
             # 180,  # randomly rotate images in the range (degrees, 0 to 180)
-            rotation_range=10,
+            rotation_range=10 if shuffle else 0,
             # 0.1,  # randomly shift images horizontally (fraction of total width)
-            width_shift_range=0.1,
+            width_shift_range=0.1 if shuffle else 0.,
             # 0.1,  # randomly shift images vertically (fraction of total height)
-            height_shift_range=0.1,
+            height_shift_range=0.1 if shuffle else 0.,
             horizontal_flip=False,  # randomly flip images
             vertical_flip=False)  # randomly flip images
 
-        batches = datagen.flow(X[idx], Y[idx], batch_size=64, shuffle=shuffle)
+        batches = datagen.flow(
+            X[idx], Y[idx], batch_size=BATCH_SIZE, shuffle=False)
         idx0 = 0
         for batch in batches:
             idx1 = idx0 + batch[0].shape[0]
@@ -88,10 +96,11 @@ def createGenerator(X, I, Y, shuffle=True):
 images_train, targets_train, positions_train = prep_data('train')
 images_test, targets_test, positions_test = prep_data('test')
 
-net = keras.models.Sequential()
 
+# merge samples, two input must be same shape
 model = keras.models.Sequential()
-model.add(keras.layers.Conv2D(filters=56, kernel_size=(5, 5), strides=2, activation='relu',
+model.add(keras.layers.Conv2D(filters=56, kernel_size=(5, 5),
+                              strides=2, activation='relu',
                               input_shape=(28, 28, 1)))
 model.add(keras.layers.Dropout(.3))
 model.add(keras.layers.Conv2D(filters=128, kernel_size=(
@@ -104,31 +113,35 @@ model.add(keras.layers.Conv2D(
 model.add(keras.layers.Flatten())
 model.add(keras.layers.Dense(units=128, activation='relu'))
 
-extra = keras.model.Sequential()
-extra.add(keras.layers.Dense(units=128, activation='relu'))
+extra = keras.models.Sequential()
+extra.add(keras.layers.Dense(input_shape=(3,), units=128, activation='relu'))
 
-net.add(keras.layers.core.Merge([model, extra], mode='concat', concat_axis=2))
-net.add(keras.layers.Dense(units=num_classes, activation='softmax'))
+conc = keras.layers.multiply(inputs=[model.output, extra.output])
+
+out = keras.layers.Dense(num_classes, activation='relu')(conc)
+
+net = keras.models.Model([model.input, extra.input], out)
+
 net.compile(loss='categorical_crossentropy',
-              optimizer='Adam', metrics=['categorical_accuracy'])
+            optimizer='Adam', metrics=['categorical_accuracy'])
 net.summary()
 
 
 training_data_generator = createGenerator(images_train, positions_train,
-                                       targets_train, shuffle=True)
+                                          targets_train, shuffle=True)
 validation_data_generator = createGenerator(images_test, positions_test,
-                                       targets_test, shuffle=True)
+                                            targets_test, shuffle=False)
 
 
-#training_data_generator = data_generator_train.flow(
-#    imgs_train, targets_train)  # , subset='training')
-#validation_data_generator = data_generator_valid.flow(
-#    imgs_test, targets_test, shuffle=False)  # , subset='validation')
+n_steps = targets_test.shape[0] // BATCH_SIZE + 1
 history = net.fit_generator(training_data_generator,
-                              steps_per_epoch=500, epochs=5,
-                              validation_data=validation_data_generator)
+                            steps_per_epoch=500, epochs=2,
+                            validation_data=validation_data_generator,
+                            validation_steps=n_steps,
+                            )
 
-Y_pred = net.predict_generator(validation_data_generator)
+Y_pred = net.predict_generator(validation_data_generator,
+                               steps=n_steps)
 y_pred = np.argmax(Y_pred, axis=1)
 y_test = np.argmax(targets_test, axis=1)
 print(y_test.shape, y_pred.shape, Y_pred.shape, targets_test.shape)
@@ -139,11 +152,9 @@ print(confusion_matrix(np.argmax(targets_test, axis=1), y_pred))
 print('Classification Report')
 target_names = CHESS_CHARS
 print(classification_report(np.argmax(targets_test, axis=1),
-                            y_pred, target_names=target_names))
-
-
+                            y_pred,
+                            target_names=target_names))
 # Plot non-normalized confusion matrix
 plot_confusion_matrix(y_test, y_pred, classes=CHESS_CHARS,
                       title='Confusion matrix, without normalization')
-
 plt.show()
